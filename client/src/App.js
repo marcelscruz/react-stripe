3fe74ec4f6f5b1873b8769817206f1cb2c11e36a
Marcel Cruz <marcelcruz@live.com>
2018-06-30 19:49:19 +0100

    get with axios added

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import axios from 'axios'
 
 class App extends Component {
+  state = {
+    response: '',
+  }
+
+  componentDidMount() {
+    this.callApi()
+  }
+
+  callApi = async () => {
+    axios
+      .get('/api/hello')
+      .then(async response => {
+        this.setState({ response: response.data.express })
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
+  // callApi = async () => {
+  //   const response = await fetch('/api/hello')
+  //   const body = await response.json()
+
+  //   if (response.status !== 200) throw Error(body.message)
+  //   console.log(response)
+  //   console.log(body)
+  //   return body
+  // }
+
   render() {
     return (
       <div className="App">
@@ -10,12 +40,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro">{this.state.response}</p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
